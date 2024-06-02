e766199588dc47d2772514f7cb2124a9aca678a3
Tejiri Adebawojo <tejirioghenekaro@gmail.com>
2024-06-02 21:49:56 +0100

    Add Week2 Assignment

---
+++ app.js
@@ -0,0 +1,42 @@
+
+const images = [
+    {
+      src: "https://cdn.pixabay.com/photo/2016/08/16/19/09/forest-1598756_1280.jpg",
+      alt: "A nice green forest",
+      width: "20vw",
+      height: "10vh",
+      id: image1,
+    },
+    { src: "https://cdn.pixabay.com/photo/2017/05/31/18/38/sea-2361247_1280.jpg",
+    alt: "A turtle in the sea",
+    width: "20vw",
+    height: "10vh",
+    id: image2,
+},
+
+    { src: "https://cdn.pixabay.com/photo/2014/05/26/13/32/butterfly-354528_1280.jpg",
+    alt: "An orange butterfly",
+    width: "20vw",
+    height: "10vh",
+    id: image3,
+},
+
+    { src: "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_1280.jpg",
+    alt: "The sunset over the sea",
+    width: "20vw",
+    height: "10vh",
+    id: image4,
+},
+
+    { src: "https://cdn.pixabay.com/photo/2018/06/29/23/27/poppies-3506807_1280.jpg",
+    alt: "A lovely red flowers",
+    width: "20vw",
+    height: "10vh"
+    id: image5,
+},
+  ]
+
+  let thumbNailContainer = document.querySelector("#thumbnail-box");
+console.log(thumbNailContainer);
+let mainPicture = document.querySelector(".body-box");
+console.log(mainPicture);
